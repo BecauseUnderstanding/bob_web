4a55a323abb38e1b5d76c3763f02034534acb257
bob <2294555287@qq.com>
2016-06-16 10:43:37 +0800

    branch Log

---
+++ Common/Common/SoaDataPage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Common
+{
+    /// <summary>
+    /// 公用model
+    /// </summary>
+    public class SoaDataPage
+    {
+        /// <summary>
+        /// 当前页
+        /// </summary>
+        public int PageIndex { get; set; }
+        /// <summary>
+        /// 每页条数
+        /// </summary>
+        public int PageSize { get; set; }
+        /// <summary>
+        /// 排序字段
+        /// </summary>
+        public string OrderBy { get; set; }
+        /// <summary>
+        /// 升降序
+        /// </summary>
+        public string SortCol { get; set; }
+    }
+
+    /// <summary>
+    /// 升降序枚举
+    /// </summary>
+    public enum Sort
+    {
+        [Display(Name = "升序")]
+        Asc = 0,
+        [Display(Name = "降序")]
+        Desc = 1
+
+
+    }
+
+    /// <summary>
+    /// 返回分页信息
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public class SoaDataPageResponse<TEntity>
+    {
+        /// <summary>
+        /// 返回行数
+        /// </summary>
+        public int Count { get; set; }
+
+        /// <summary>
+        /// 返回信息
+        /// </summary>
+        public IEnumerable<TEntity> Body { get; set; }
+    }
+
+}
+
+
---
+++ Common/Log/ILog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Log
+{
+    /// <summary>
+    /// 日志接口
+    /// </summary>
+   public interface ILog
+    {
+       /// <summary>
+       /// 写日志
+       /// </summary>
+       /// <param name="level">日志级别</param>
+       /// <param name="title">日志标题</param>
+       /// <param name="appId">应用程序id</param>
+       /// <param name="ip">IP地址</param>
+       /// <param name="userid">用户id</param>
+       /// <param name="createTime">创建时间</param>
+       /// <param name="total">参数</param>
+       /// <param name="message">日志内容</param>
+       /// <param name="source">来源</param>
+       /// <param name="stackTrace">堆栈信息</param>
+        void WriteLog(LogLevel level,string title,int appId,string ip,int userid,DateTime createTime,string total,string message,string source,string stackTrace);
+    }
+}
